bb71c291ec36bc90d315b55ac78c403625a4fa59
yeshaobin <983281057@qq.com>
2020-11-01 17:19:58 +0800

    2020-11-1

---
+++ mooc/graph/dfs.js
@@ -0,0 +1,12 @@
+const graph = require('./graph')
+const visited = new Set()
+function dfs(n){
+  console.log(n);
+  visited.add(n);
+  graph[n].forEach(c=>{
+    if(!visited.has(c)){
+      dfs(c)
+    }
+  })
+}
+dfs(2)
---
+++ mooc/graph/graph.js
@@ -0,0 +1,8 @@
+const graph = {
+  0:[1,2,3],
+  1:[2,3],
+  2:[0,3],
+  3:[3],
+ 
+}
+module.exports = graph;
